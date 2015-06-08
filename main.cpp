bfb46b6e8c6829e97abcc07a6d9d153fbc4f9e1c
Dor Cohen <dordchn@gmail.com>
2015-06-08 21:08:04 +0300

    Initial checkin

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jun 8, 2015
+ *      Author: dor
+ */
+
+#include <iostream>
+
+//#include "Objects/Fruit.h"
+using namespace std;
+
+int main(int argc, const char**argv) {
+	cout << "hello world" << endl;
+	return 0;
+}
